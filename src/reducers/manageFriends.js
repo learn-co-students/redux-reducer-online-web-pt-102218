dba16c69f059757a26a5ac3c7c593fbc8441e151
Paul Ashour <paul.ashour@gmail.com>
2019-08-12 15:43:01 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  let newState = {...state}
+  switch (action.type) {
+    case "ADD_FRIEND":
+      newState.friends = [...newState.friends, action.friend]
+      return newState;
+    case "REMOVE_FRIEND":
+      newState.friends = [...newState.friends.filter(v => v.id !== action.id)]
+      return newState;
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case "INCREASE":
+      let newState = {...state, numberOfPresents: state.numberOfPresents + 1}
+      return newState
+    default:
+      return state;
+  }
 }
